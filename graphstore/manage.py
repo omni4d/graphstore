6f55bd0ab184d6b5f3147c517b20ca80c438451d
Owen Campbell <owen.campbell@empiria.co.uk>
2016-07-29 09:18:12 +0100

    Use python dict for environment variables

--- graphstore/manage.py
+++ graphstore/manage.py
@@ -1,33 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
-import re
 
 
-def read_env():
-    """Pulled from Honcho code with minor updates, reads local default
-    environment variables from a .env file located in the project root
-    directory.
-
-    """
+def read_environment():
     try:
-        with open('.env') as f:
-            content = f.read()
-    except IOError:
+        from environment import ENVIRONMENT
+    except ImportError:
         return
 
-    for line in content.splitlines():
-        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
-        if m1:
-            key, val = m1.group(1), m1.group(2)
-            m2 = re.match(r"\A'(.*)'\Z", val)
-            if m2:
-                val = m2.group(1)
-            m3 = re.match(r'\A"(.*)"\Z', val)
-            if m3:
-                val = re.sub(r'\\(.)', r'\1', m3.group(1))
-                print('hello')
-            os.environ.setdefault(key, val)
+    for variable, value in ENVIRONMENT.items():
+        os.environ.setdefault(variable, str(value))
 
 
 if __name__ == "__main__":
@@ -35,5 +18,5 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-    read_env()
+    read_environment()
     execute_from_command_line(sys.argv)
